0907c9fdb034e7db321bde6526b8385890967e9d
nhlee98 <e5slnh9876@gmail.com>
2025-09-01 19:42:38 +0900

    refactor: 결제 조회 기능 삭제(인터뷰 서버로 이동)

--- domain/src/main/java/com/samhap/kokomen/payment/repository/TosspaymentsPaymentResultRepository.java
+++ domain/src/main/java/com/samhap/kokomen/payment/repository/TosspaymentsPaymentResultRepository.java
@@ -1,20 +1,10 @@
 package com.samhap.kokomen.payment.repository;
 
-import com.samhap.kokomen.payment.domain.PaymentState;
 import com.samhap.kokomen.payment.domain.TosspaymentsPaymentResult;
-import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TosspaymentsPaymentResultRepository extends JpaRepository<TosspaymentsPaymentResult, Long> {
-
-    Page<TosspaymentsPaymentResult> findByTosspaymentsPaymentMemberIdAndTosspaymentsPaymentStateIn(
-            Long memberId,
-            List<PaymentState> states,
-            Pageable pageable
-    );
 
     Optional<TosspaymentsPaymentResult> findByTosspaymentsPaymentId(Long tosspaymentsPaymentId);
 }
